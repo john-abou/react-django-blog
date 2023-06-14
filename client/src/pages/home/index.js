5bd7f6e43572c7ae1da344e385004af0c24936a3
john-abou <abounassar.john@gmail.com>
2023-06-13 23:12:13 -0400

    Basic client side file structure

---
+++ client/src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Home</h1>
+      <Link to="/login">Login</Link>
+    </div>
+  );
+}
