e8a52235001074756d73b5ce25a3a48063734385
john-abou <abounassar.john@gmail.com>
2023-06-20 23:27:21 -0400

    Dynamic routing using route names django.urls feat

--- server/challenges/urls.py
+++ server/challenges/urls.py
@@ -7,9 +7,12 @@ urlpatterns = [
     # dynamic path segment uses <>, anything req params are passed to the monthly_challenge function as an argument
     # Adding a data type to the dynamic path segment: <str:month> -- this is a string data type -- input validation
     # Order of the url patterns matters, Django will go through the list of url patterns and use the first one that matches the route
+    # If the route is /challenges/january, Django will use the first url pattern that matches the route, which is the first one in the list
+    # name is a keyword arg, used to make the url pattern dynamic. Now if the route changes in the server/urls.py file, the url pattern will still work
+    # django uses the route 'name' to find the url pattern. In the views.py file, the reverse() function uses the 'name' to find the url pattern
     path('hello_world', views.hello_world),
     path('test', views.test),
-    path('<int:month>', views.monthly_challenge_by_number),
-    path('<str:month>', views.monthly_challenge),
+    path('<int:month>', views.monthly_challenge_by_number), # Dynamic path segment is an integer
+    path('<str:month>', views.monthly_challenge, name='month-challenge'), # Dynamic path segment is a string 
 ]
 
--- server/challenges/views.py
+++ server/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     'january': 'Eat no meat for the entire month!',
@@ -36,6 +37,15 @@ def monthly_challenge_by_number(req, month):
     forward_month_array = list(monthly_challenges.keys()) # Get a list of the keys of the monthly_challenges dictionary
     try:
         forward_month = forward_month_array[month - 1] # Get the month from the list of keys using the month param as the index
-        return HttpResponseRedirect('/challenges/' + forward_month) # Redirect to the monthly_challenge view using the month as the dynamic path segment
+
+        # Use the reverse() function to get the url pattern for the month-challenge url pattern
+        # This is the path to the month-challenge url pattern -- /challenges/<str:month>
+        # Note, the reverse() function returns the path as a string
+        redirect_path = reverse('month-challenge', args=[forward_month]) 
+
+        #Redirect to the monthly_challenge view using the month as the dynamic path segment
+        #Note, the re-direct is not relative to the current route, it is relative to the root route of the server (server/urls.py)
+        # This is why we need to add the 'challenges/' part of the route to the redirect
+        return HttpResponseRedirect(redirect_path)
     except:
         return HttpResponseNotFound('This month is not supported!')  
