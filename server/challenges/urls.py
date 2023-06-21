104fb8f09789e37963414bc7040698573f6aa9f5
john-abou <abounassar.john@gmail.com>
2023-06-20 23:10:54 -0400

    Dynamic routing and re-routing in django

--- server/challenges/urls.py
+++ server/challenges/urls.py
@@ -4,7 +4,12 @@ from . import views # import the views file from the same directory
 urlpatterns = [
     # Example of a url configuration, route is '/challenges/january', controller is function called index from views file 
     # '/challenges' part of the route is set up in the project (server directory) urls.py file. Think of this as a sub-route -- abstraction
-    path('january', views.index),
-    path('february', views.feb),
-    path('<month>', views.monthly_challenge) # dynamic path segment uses <>, anything req params are passed to the monthly_challenge function as an argument
+    # dynamic path segment uses <>, anything req params are passed to the monthly_challenge function as an argument
+    # Adding a data type to the dynamic path segment: <str:month> -- this is a string data type -- input validation
+    # Order of the url patterns matters, Django will go through the list of url patterns and use the first one that matches the route
+    path('hello_world', views.hello_world),
+    path('test', views.test),
+    path('<int:month>', views.monthly_challenge_by_number),
+    path('<str:month>', views.monthly_challenge),
 ]
+
--- server/challenges/views.py
+++ server/challenges/views.py
@@ -1,22 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
-# Create your views here.
-def index(req):
+monthly_challenges = {
+    'january': 'Eat no meat for the entire month!',
+    'february': 'Walk for at least 20 minutes every day!',
+    'march': 'Learn Django for at least 20 minutes every day!',
+    'april': 'Eat no meat for the entire month!',
+    'may': 'Walk for at least 20 minutes every day!',
+    'june': 'Learn Django for at least 20 minutes every day!',
+    'july': 'Eat no meat for the entire month!',
+    'august': 'Walk for at least 20 minutes every day!',
+    'september': 'Learn Django for at least 20 minutes every day!',
+    'october': 'Eat no meat for the entire month!',
+    'november': 'Walk for at least 20 minutes every day!',
+    'december': 'Learn Django for at least 20 minutes every day!',
+}
+
+# Create your views here. Note, no need to export the views file or functions from the views file. 
+# Django will find the views file and use the functions in it.
+def hello_world(req):
     return HttpResponse('Hello, world. You are at the challenges index.')
 
-def feb(req):
+def test(req):
     return HttpResponse('This is the February challenge.')
 
 def monthly_challenge(req, month): # req is the request object, month is the dynamic path segment (req param(s) -- keyword arguments)
-    challenge_text = None
-    if month == 'april':
-        challenge_text = 'Eat no meat for the entire month!'
-    elif month == 'may':
-        challenge_text = 'Walk for at least 20 minutes every day!'
-    elif month == 'june':
-        challenge_text = 'Learn Django for at least 20 minutes every day!'
-    else:
-        return HttpResponseNotFound('This month is not supported!')     
-    
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound('This month is not supported!')  
+
+
+def monthly_challenge_by_number(req, month):
+    forward_month_array = list(monthly_challenges.keys()) # Get a list of the keys of the monthly_challenges dictionary
+    try:
+        forward_month = forward_month_array[month - 1] # Get the month from the list of keys using the month param as the index
+        return HttpResponseRedirect('/challenges/' + forward_month) # Redirect to the monthly_challenge view using the month as the dynamic path segment
+    except:
+        return HttpResponseNotFound('This month is not supported!')  
